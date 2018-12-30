bb2cf3992d3772b0b94c77a599f43045821bedd0
Justin Artis <jusart1s8@gmail.com>
2018-12-30 18:42:33 -0500

    added base changes for loading screen and for main activty look
    added style configurations as well

---
+++ app/src/main/java/com/example/artisja/genwatch/LoadingActivity.java
@@ -0,0 +1,33 @@
+package com.example.artisja.genwatch;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(10000);  //Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(LoadingActivity.this,
+                            MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
